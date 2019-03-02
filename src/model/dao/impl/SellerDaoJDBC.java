93464ca68e56fab547f38506a47e3d70a2115256
Italo Jose <italo.ads31@gmail.com>
2019-03-02 18:14:30 -0300

    Create DaoFactory and SellerDao implements  and implements the FindById

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,93 @@
+package model.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DBException;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	public Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement ps =null;
+		ResultSet rs = null;
+		
+		try {
+			ps = conn.prepareStatement("Select s.*, d.name "
+					+ "as DepName from Seller s "
+					+ "join "
+					+ "Department d "
+					+ " on d.id=s.departmentId"
+					+ " where s.id=?");
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				Department dep = new Department();
+				
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+				
+				Seller sel = new Seller();
+				
+				sel.setId(rs.getInt("Id"));
+				sel.setName(rs.getString("Name"));
+				sel.setBaseSalary(rs.getDouble("baseSalary"));
+				sel.setDepartment(dep);
+				sel.setEmail(rs.getString("email"));
+				sel.setBirthDate(rs.getDate("BirthDate"));
+				
+				return sel;
+				
+			}return null;
+						
+		}catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(ps);
+		}
+		
+		
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		
+		return null;
+	}
+
+}
